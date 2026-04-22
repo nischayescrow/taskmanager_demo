6165ebdb4f8fbfa77a2234ab8a5bfb06e0191fd4
nischayescrow <nischay.escrow@gmail.com>
2026-04-22 18:41:43 +0530

    loki production changes

--- src/config/winston/winston.config.ts
+++ src/config/winston/winston.config.ts
@@ -1,5 +1,6 @@
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as winston from 'winston';
+import LokiTransport from 'winston-loki';
 
 export const winstonConfig: winston.LoggerOptions = {
   transports: [
@@ -21,6 +22,17 @@ export const winstonConfig: winston.LoggerOptions = {
         winston.format.timestamp(),
         winston.format.json(),
       ),
+    }),
+
+    // Direct push to Grafana Cloud Loki
+    new LokiTransport({
+      host: process.env.GRAFANA_LOKI_URL!,
+      basicAuth: `${process.env.GRAFANA_LOKI_USER!}:${process.env.GRAFANA_LOKI_TOKEN!}`, // UserID:Token
+      labels: { job: 'nestjs-app' },
+      json: true,
+      format: winston.format.json(),
+      replaceTimestamp: true,
+      onConnectionError: (err) => console.error('Loki Connection Error:', err),
     }),
   ],
 };
