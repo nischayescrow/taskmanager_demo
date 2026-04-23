ae629bb6b622e32c7dd4d9a97eb0d08720eb48af
nischayescrow <nischay.escrow@gmail.com>
2026-04-23 12:43:38 +0530

    log issue fix 1.8

--- src/config/winston/winston.config.ts
+++ src/config/winston/winston.config.ts
@@ -2,10 +2,19 @@ import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as winston from 'winston';
 import LokiTransport from 'winston-loki';
 
+const removeNullStack = winston.format((info) => {
+  if (info.stack && Array.isArray(info.stack) && info.stack[0] === null) {
+    delete info.stack;
+  }
+  return info;
+});
+
 export const winstonConfig: winston.LoggerOptions = {
+  level: 'debug',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
+    removeNullStack(),
     winston.format.json(),
   ),
   transports: [
@@ -25,7 +34,7 @@ export const winstonConfig: winston.LoggerOptions = {
       labels: { job: 'nestjs-app' },
       json: true,
       batching: false,
-      replaceTimestamp: true,
+      replaceTimestamp: false,
       onConnectionError: (err) => {
         console.log('--- LOKI DEBUG START ---');
         console.error(err);
